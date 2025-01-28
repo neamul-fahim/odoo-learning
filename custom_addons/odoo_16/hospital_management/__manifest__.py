a37852ffa88c728f443d8c5d2083dac0c26786cf
neamul-fahim <neamul.fahim@gmail.com>
2025-01-28 16:11:01 +0600

    appended menu.xml at the last of data in manifest file of hospital management

--- custom_addons/odoo_16/hospital_management/__manifest__.py
+++ custom_addons/odoo_16/hospital_management/__manifest__.py
@@ -9,11 +9,12 @@
         'data/sequence.xml',
         'reports/patient_bill_report_action.xml',
         'reports/patient_bill_report_template.xml',
-        'views/menu.xml',
         'views/hospital_patient_views.xml',
         'views/hospital_patient_bill_view.xml',
         'views/hospital_doctor_view.xml',
         'views/hospital_department_views.xml',
+        'views/menu.xml', #keep the menu at last so that all the other files can load before it!!!
+
     ]
 }
 
